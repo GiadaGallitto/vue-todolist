f205758b005c370a94d48cc61dbc3bfa18bf3727
GiadaGallitto <gallitto.giada@gmail.com>
2022-12-14 15:56:01 +0100

    Add element on DOM and Apply line-through to done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,32 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+            worksList : [
+                {
+                    text : "Ritirare il bucato",
+                    done : false
+                },
 
+                {
+                    text : "Fare la spesa",
+                    done : false
+                },
+
+                {
+                    text : "Tagliare l'erba",
+                    done : true
+                },
+
+                {
+                    text : "Spedire il pacco",
+                    done : true
+                },
+
+                {
+                    text : "Andare in libreria",
+                    done : false
+                },
+            ]
         }
     },
 
